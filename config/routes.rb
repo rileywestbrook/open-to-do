40e6adad2bf92595f1646fe1c3dc009abf50e33d
Riley Westbrook <westbrookriley@gmail.com>
2018-04-13 12:56:27 -0400

    authenitcated? not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  namespace :api, defaults: { format: :json } do
+     resources :users
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
